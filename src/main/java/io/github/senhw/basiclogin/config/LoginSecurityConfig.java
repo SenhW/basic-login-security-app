6b87ed5a2888166fe9942a8c6daa998e384131bd
SenhW <senhwang3@gmail.com>
2019-05-05 18:53:35 -0400

    Create Security Initializer and Config and add sample users

---
+++ src/main/java/io/github/senhw/basiclogin/config/LoginSecurityConfig.java
@@ -0,0 +1,28 @@
+package io.github.senhw.basiclogin.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.User.UserBuilder;
+
+@Configuration
+@EnableWebSecurity
+public class LoginSecurityConfig extends WebSecurityConfigurerAdapter {
+
+	@Override
+	protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+
+		// add our users for in memory authentication
+		
+		UserBuilder users = User.withDefaultPasswordEncoder();
+		
+		auth.inMemoryAuthentication()
+			.withUser(users.username("jason").password("pass123").roles("EMPLOYEE"))
+			.withUser(users.username("mary").password("pass123").roles("MANAGER"))
+			.withUser(users.username("sarah").password("pass123").roles("ADMIN"));
+
+	}
+	
+}
---
+++ src/main/java/io/github/senhw/basiclogin/config/SecurityWebApplicationInitializer.java
@@ -0,0 +1,8 @@
+package io.github.senhw.basiclogin.config;
+
+import org.springframework.security.web.context.AbstractSecurityWebApplicationInitializer;
+
+public class SecurityWebApplicationInitializer 
+						extends AbstractSecurityWebApplicationInitializer {
+
+}
