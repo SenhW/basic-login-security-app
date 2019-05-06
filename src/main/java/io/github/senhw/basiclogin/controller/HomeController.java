8b8635b9aa7040bb4e40f24619089b3f27c9698c
SenhW <senhwang3@gmail.com>
2019-05-05 23:14:28 -0400

    Create custom login form and request mapping

--- src/main/java/io/github/senhw/basiclogin/controller/HomeController.java
+++ src/main/java/io/github/senhw/basiclogin/controller/HomeController.java
@@ -4,12 +4,11 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
-public class LoginController {
+public class HomeController {
 
-	@GetMapping("/showMyLoginPage")
-	public String showMyLoginPage() {
+	@GetMapping("/")
+	public String showHome() {
 		
-		return "login-page";
-		
+		return "home";
 	}
 }
