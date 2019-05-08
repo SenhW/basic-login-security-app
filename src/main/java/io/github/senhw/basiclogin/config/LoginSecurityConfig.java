4cdeb9b60f5064a55be2465add4766a1e72349ae
SenhW <senhwang3@gmail.com>
2019-05-07 22:34:51 -0400

    Create controller and view for custom access denied page

--- src/main/java/io/github/senhw/basiclogin/config/LoginSecurityConfig.java
+++ src/main/java/io/github/senhw/basiclogin/config/LoginSecurityConfig.java
@@ -39,7 +39,9 @@ public class LoginSecurityConfig extends WebSecurityConfigurerAdapter {
 				.loginProcessingUrl("/authenticateTheUser")
 				.permitAll()
 			.and()
-			.logout().permitAll();
+			.logout().permitAll()
+			.and()
+			.exceptionHandling().accessDeniedPage("/access-denied");
 		
 	}
 	
--- src/main/java/io/github/senhw/basiclogin/controller/LoginController.java
+++ src/main/java/io/github/senhw/basiclogin/controller/LoginController.java
@@ -12,4 +12,13 @@ public class LoginController {
 		return "fancy-login-page";
 		
 	}
+	
+	// add request mapping for /access-denied
+	@GetMapping("/access-denied")
+	public String showAccessDenied() {
+		
+		return "access-denied";
+		
+	}
+	
 }
